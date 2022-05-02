48335ec2688cc8cce5e8556705c46311a3443066
Shashi Kant <skant9025@gmail.com>
2022-05-02 14:27:11 +0530

    added route code and fix home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,52 @@
-import React from 'react'
-import Nav from './Navbar/Nav'
-import Footer from './Footer/Footer'
+import React, { Component } from 'react';
+import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
 
-function App() {
-  return (
-    <>
-    <Nav/>
-    <div className='container'>
-      
-    </div>
-    <Footer/>
-    </>
-  )
+import Home from './components/home';
+import FAQs from './components/FAQs';
+
+
+
+class App extends Component {
+  render() {
+  
+    const slash = () => {
+      return(
+        <div>
+          <Home/>
+        </div>
+      );
+    }
+  
+    return (
+    <Router>
+      <Routes>
+          <Route  path='/' element={< Home />}></Route>
+          <Route exact path='/FAQs' element={< FAQs />}></Route>
+          {/* <Route exact path='/about' element={< About />}></Route> */}
+          {/* <Route exact path='/contact' element={< Contact />}></Route> */}
+      </Routes>
+    </Router>
+    );
+  }
 }
+
+// function App() {
+//   return (
+//     <>
+//     <Nav/>
+//     <div className='container'>
+      
+//     </div>
+//     <Footer/>
+
+//     <Router
+//       ><Routes>
+//         <Route  path='/' element={< Nav />}></Route>
+//         <Route exact path='/FAQs' element={< FAQs />}></Route>
+//       </Routes>
+//     </Router>
+//     </>
+//   )
+// }
 
 export default App
